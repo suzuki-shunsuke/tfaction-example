429c835fced9f46325e13588e79921c0ebe7f769
Nimesha-Premaraja <prem.nimesha@prodoscore.com>
2023-11-27 11:50:38 +0530

    chore: test setup

--- gcp/dev/vm3/main.tf
+++ gcp/dev/vm3/main.tf
@@ -1,5 +1,5 @@
 resource "random_string" "random" {
-  length           = 16
+  length           = var.length
   special          = true
   override_special = "/@£$"
 }
---
+++ gcp/dev/vm3/varaibles.tf
@@ -0,0 +1,3 @@
+variable "length" {
+  default =   16
+}
