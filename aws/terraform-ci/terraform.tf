cfa68b376d89909d814c9cec64d00b9fc1042250
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2024-02-10 09:56:43 +0900

    fix: fix lint errors

--- aws/terraform-ci/terraform.tf
+++ aws/terraform-ci/terraform.tf
@@ -1,8 +1,13 @@
 terraform {
+  required_version = ">= 1.0"
   required_providers {
     aws = {
       source  = "hashicorp/aws"
       version = "~> 5.0"
+    }
+    tls = {
+      source  = "hashicorp/tls"
+      version = "4.0.5"
     }
   }
 
