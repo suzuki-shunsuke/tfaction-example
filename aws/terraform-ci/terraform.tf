e47a2583e11df5c823772324d4acba07d52c0375
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2024-02-10 10:05:24 +0900

    comment out resources

--- aws/terraform-ci/terraform.tf
+++ aws/terraform-ci/terraform.tf
@@ -5,10 +5,10 @@ terraform {
       source  = "hashicorp/aws"
       version = "~> 5.0"
     }
-    tls = {
-      source  = "hashicorp/tls"
-      version = "4.0.5"
-    }
+    # tls = {
+    #   source  = "hashicorp/tls"
+    #   version = "4.0.5"
+    # }
   }
 
   # backend "s3" {
