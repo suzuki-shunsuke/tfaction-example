83ff75ce9b5d2394ef53b0e492da85d8bcda6a4e
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2024-10-09 20:40:03 +0900

    chore: use Hashicorp Cloud as Backend

--- aws/terraform-ci/backend.tf
+++ aws/terraform-ci/backend.tf
@@ -1,8 +1,8 @@
 terraform {
-  # CHANGEME: Uncomment the following block to enable remote backend
-  # backend "s3" {
-  #   bucket = "<S3 Bucket Name>"
-  #   key    = "aws/terraform-ci/v1/terraform.tfstate"
-  #   region = "us-east-1"
-  # }
+  cloud {
+    organization = "suzuki-shunsuke"
+    workspaces {
+      name = "aws_terraform-ci"
+    }
+  }
 }
