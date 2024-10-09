2b8342508fe063bd90186093b881784235ea40b2
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2024-10-09 22:15:38 +0900

    use tfe backend

--- github/services/foo/backend.tf
+++ github/services/foo/backend.tf
@@ -1,7 +1,8 @@
 terraform {
-  # backend "s3" {
-  #   bucket = "<S3 Bucket Name>"
-  #   key    = "github/foo/v1/terraform.tfstate"
-  #   region = "ap-northeast-1"
-  # }
+  cloud {
+    organization = "suzuki-shunsuke"
+    workspaces {
+      name = "github_foo"
+    }
+  }
 }
