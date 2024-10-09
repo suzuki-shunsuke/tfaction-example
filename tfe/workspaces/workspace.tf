b19d89de0200d1d39672acf4fa32209960bdb388
Shunsuke Suzuki <suzuki.shunsuke.1989@gmail.com>
2024-10-09 22:11:11 +0900

    create tfe workspaces for github

--- tfe/workspaces/workspace.tf
+++ tfe/workspaces/workspace.tf
@@ -7,3 +7,13 @@ resource "tfe_workspace_settings" "aws_terraform_ci" {
   workspace_id   = tfe_workspace.aws_terraform_ci.id
   execution_mode = "local"
 }
+
+resource "tfe_workspace" "github_foo" {
+  name       = "github_foo"
+  project_id = tfe_project.main.id
+}
+
+resource "tfe_workspace" "github_bar" {
+  name       = "github_bar"
+  project_id = tfe_project.main.id
+}
